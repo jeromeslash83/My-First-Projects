c139836cdd756224f7442d09385542fd850e317e
jeromeslash83 <76027883+jeromeslash83@users.noreply.github.com>
2021-01-07 19:19:27 +0800

    Add files via upload
    My First codes in my coding journey.

---
+++ BMI_Calculator.py
@@ -0,0 +1,18 @@
+print("Welcome to Jerome's BMI Calculator!")
+def BMI_calculator():
+    Name = input("What is your name? ")
+    Weight = input("What is your weight (in kgs)? ")
+    Height = input("What is your height(in m)?")
+
+    BMI = float(Weight) / (float(Height) * float(Height))
+
+    print("Your BMI is", BMI)
+
+    if BMI < 18.5:
+        print(Name, "You are underweight.")
+    elif BMI >= 18.5 or BMI == 24.9:
+        print(Name, "You're normal.")
+    elif BMI >=25.0 or BMI == 30.0:
+        print(Name, "You're overweight.")
+    else:
+        print(Name, "You're obese."
