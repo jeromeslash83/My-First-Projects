14d7773875c30f7a2349b9e10a44d597c8b74a2c
jeromeslash83 <76027883+jeromeslash83@users.noreply.github.com>
2021-01-08 09:46:51 +0800

    Add files via upload

---
+++ Bill_and_tip_splitter.py
@@ -0,0 +1,13 @@
+print("Welcome to Jerome's tip calculator.")
+while True:
+    bill = input("What was the total bill? ")
+    split = input("How many people will split the bill? ")
+    tip = input("What percentage of tip would you like to give? (10, 12, or 15) ")
+    bill_int = int(bill)
+    split_int = int(split)
+    tip_int = int(tip) / 100
+    total = ((bill_int * tip_int) + bill_int) / split_int
+    print("Your total bill for each person is:", total)
+    continue_program = input("Do you want to make a calculation again? (Y/N) ")
+    if continue_program == "N":
+        break
