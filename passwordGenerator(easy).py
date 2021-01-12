95e7c35171e7cfd7d82393bb3e20b66375b2f97d
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-01-12 10:53:36 +0800

    Create passwordGenerator(easy).py

---
+++ passwordGenerator(easy).py
@@ -0,0 +1,29 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+alphabet = random.sample(letters, nr_letters)
+integer = random.sample(numbers, nr_numbers)
+symbols1 = random.sample(symbols, nr_symbols)
+
+combi = ''
+for let in alphabet:
+    combi += let
+
+n_combi = ''
+for num in integer:
+    n_combi += num
+
+s_combi = ''
+for sym in symbols1:
+    s_combi += sym
+
+password = combi + n_combi + s_combi
+print(password)
