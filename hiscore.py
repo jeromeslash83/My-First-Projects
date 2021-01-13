a2bee88955179b579e20e9d3f58d2ed907d41bca
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-01-13 22:26:48 +0800

    Update hiscore.py

--- hiscore.py
+++ hiscore.py
@@ -1,9 +1,17 @@
 student_scores = input("Input a list of student scores ").split()
-for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
-print(student_scores)
-x = student_scores[0]
-for score in student_scores:
-  if score > x:
-    x = score
-print(f'The highest score in the class is: {x}')
+while True:  
+  for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+  print(student_scores)
+  x = student_scores[0]
+  for score in student_scores:
+    if score > x:
+      x = score
+  print(f'The highest score in the class is: {x}')
+  continuation = input('Do you want to continue? (y/n)')
+  if continuation == 'y':
+    continue
+  elif continuation == 'n':
+    break
+  else:
+    print('Wrong input. Try again')
