47c1e852601e926d6034065f6afd02c069ad02e2
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-01-14 21:41:42 +0800

    Update Hangman_project.py

--- Hangman_project.py
+++ Hangman_project.py
@@ -2,7 +2,9 @@ word_list = ["aardvark", "baboon", "camel"]
 import random
 chosen_word = random.choice(word_list)
 guess = input('Guess a letter from the given word: ')
-if guess.lower() in chosen_word.lower:
-  print('Yes') 
-else:
-  print('No')
+numbers = int(len(chosen_word))
+for n in range(0, numbers):
+    if guess.lower() in chosen_word.lower()[n]:
+        print('Yes') 
+    else:
+        print('No')
