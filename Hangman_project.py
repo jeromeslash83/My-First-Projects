93aaf3337d2935da53ac585559594eb7c9369e50
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-01-16 16:25:56 +0800

    Update Hangman_project.py

--- Hangman_project.py
+++ Hangman_project.py
@@ -9,11 +9,29 @@ display = []
 for n in chosen_word:
     display.append('_')
 print(display)
-guess = input("Guess a letter: ").lower()
+lives = []
+loser = 'HANGMAN'
+win = 0
+lose = 0
 number = int(len(chosen_word))
-
-for n in range(number) :
-    letter = chosen_word[n]
-    if letter == guess:
-         display[n] = guess
-print(display)
+while win != number:
+    guess = input("Guess a letter: ").lower()
+    for n in range(number) :
+        letter = chosen_word[n]
+        if letter == guess:
+            display[n] = guess
+            win += 1
+    if not guess in chosen_word:
+        lose += 1
+        for i in range(1,8):
+            if lose == i:
+                lives.append(loser[i-1])
+    
+    if lose == 7:
+        print('You Lose.')
+        break
+    print(f'Correct answer:\n{display}\n\n')
+    print(f'Lives:\n{lives}\n')
+else:
+    if win == number:
+        print('You Win.')
