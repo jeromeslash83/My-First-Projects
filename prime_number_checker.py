090ffeb2376b2386135ccacd45b08b5717710301
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-01-19 20:08:22 +0800

    Update prime_number_checker.py

--- prime_number_checker.py
+++ prime_number_checker.py
@@ -1,19 +1,15 @@
-divis = [2, 3, 5, 7]
-def prime_checker(number):
-  if n % n == 0:
-    if n in divis:
-      print("It's a prime number.")
-    elif n not in divis:
-      if n % 2 == 0:
-        print("It's not a prime number.")
-      elif n % 3 == 0:
-        print("It's not a prime number.")
-      elif n % 5 == 0:
-        print("It's not a prime number.")
-      elif n % 7 == 0:
-        print("It's not a prime number.")
-      else:
-        print("It's a prime number.")
+#PRIME NUMBER CHECKER
+while True:
+    def prime_checker(number):
+        for i in range (2, number-1):
+            prime = True
+            if number % i == 0:
+                prime = False
+        if prime:
+            print("It's a prime number.")
+        else:
+            print("It's not a prime number.")
+        
 
-n = int(input("Check this number: "))
-prime_checker(number=n)
+    n = int(input("Check this number: "))
+    prime_checker(number=n)
