9a382ea0046e32c691ea01d68a51a9b5b60f54c0
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-01-25 13:15:50 +0800

    Update Calculator.py

--- CalculatorCode/Calculator.py
+++ CalculatorCode/Calculator.py
@@ -11,47 +11,47 @@ def multiply(n1, n2):
 def divide(n1, n2):
   return n1 / n2
 
+def calculator():
+  n1 = int(input("What's the first number? "))
+  loop = True
+  while loop:
 
-loop = True
-n1 = int(input("What's the first number? "))
-
-while loop:
-
-  operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide
-  }
-  
-  for symbol in operations:
-    print(symbol)
-
-  to_do = input("What operation are we gonna do? ")
-  if to_do in operations:
-    operational = True
-  else:
-    print('Wrong input try again.')
-    to_do = input("What operation are we gonna do? ")
+    operations = {
+      "+": add,
+      "-": subtract,
+      "*": multiply,
+      "/": divide
+    }
     
-  n2 = int(input("What's the next number? "))
+    for symbol in operations:
+      print(symbol)
 
-  answer = operations[to_do](n1, n2)
-
-  print(f"{n1} {to_do} {n2} = {answer}")
-
-  cont = input(f"Do you want to continue with calculations with {answer}?\n'y' or 'n': ")
-  if cont.lower() == 'y':
-    n1 = answer
-  elif cont.lower() == 'n':
-    calc = input("Do you want to start a new calculation? 'y' or 'n':\n")
-    if calc.lower() == 'n':
-      loop = False
-    elif calc.lower() == 'y':
-      n1 = int(input("What's the first number? "))
+    to_do = input("What operation are we gonna do? ")
+    if to_do in operations:
+      ops = to_do
     else:
-      print('Wrong input. Try again')
+      print('Wrong input try again.')
+      to_do = input("What operation are we gonna do? ")
+      
+    n2 = int(input("What's the next number? "))
+
+    answer = operations[to_do](n1, n2)
+
+    print(f"{n1} {ops} {n2} = {answer}")
+
+    cont = input(f"Do you want to continue with calculations with {answer}?\n'y' or 'n': ")
+    if cont.lower() == 'y':
+      n1 = answer
+    elif cont.lower() == 'n':
       calc = input("Do you want to start a new calculation? 'y' or 'n':\n")
-  else:
-    print("Wrong input try again.")
-    cont = input(f"Do you want to continue with calculations with {answer}?\n'y' or 'n': y")
+      if calc.lower() == 'n':
+        loop = False
+      elif calc.lower() == 'y':
+        calculator()
+      else:
+        print('Wrong input. Try again')
+        calc = input("Do you want to start a new calculation? 'y' or 'n':\n")
+    else:
+      print("Wrong input try again.")
+      cont = input(f"Do you want to continue with calculations with {answer}?\n'y' or 'n': y")
+calculator()
