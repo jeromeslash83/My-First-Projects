e5983efaabb406689349d6ca63823ccacba8f2e4
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-01-29 20:38:12 +0800

    Update alternative_code.py

--- basic_games/Blackjack/alternative_code.py
+++ basic_games/Blackjack/alternative_code.py
@@ -3,6 +3,10 @@ import random
 cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 print("Welcome to BlackJack!")
 
-def deal_card():
-  card = random.choice(card)
-  return card
+def deal():
+    card = random.choice(cards)
+    return card
+
+def addition(card1, card2):
+    total = card1 + card2
+    return total
