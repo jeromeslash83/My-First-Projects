24502399a4007fe7952fa75590ecdf863940a265
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-01-29 21:42:52 +0800

    Update alternative_code.py

--- basic_games/Blackjack/alternative_code.py
+++ basic_games/Blackjack/alternative_code.py
@@ -1,4 +1,5 @@
 #BLACKJACK GAME ALTERNATIVE CODE
+#A little bit simplified compared to the other one
 import random
 def blackjack():
     cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K","A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
