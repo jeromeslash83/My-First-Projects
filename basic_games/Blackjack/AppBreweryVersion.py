f9078fe7a238a0c59a7e3fd3719f2003a42b39e9
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-01 10:01:06 +0800

    Update AppBreweryVersion.py

--- basic_games/Blackjack/AppBreweryVersion.py
+++ basic_games/Blackjack/AppBreweryVersion.py
@@ -1,3 +1,20 @@
 #Angela's code in the course
 import random
 import blackjack_art
+from replit import clear
+
+def deal_card():
+  """Returns a random card from the deck of cards"""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+player_card = []
+dealer_card = []
+for _ in range(2):
+  dealt_card = deal_card()
+  player_card.append(dealt_card)
+
+dealer_card.append(deal_card())
+  
+
