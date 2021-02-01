d5ca34630ca40195a7e3396d3d1ff27aa5efb9b0
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-01 10:12:38 +0800

    Update AppBreweryVersion.py

--- basic_games/Blackjack/AppBreweryVersion.py
+++ basic_games/Blackjack/AppBreweryVersion.py
@@ -9,6 +9,18 @@ def deal_card():
   card = random.choice(cards)
   return card
 
+def calculate_score(cards):
+  """Takes a list of cards as an input and calculates their score"""
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  
+  if 11 in cards and sum(cards) > 21:
+    cards.remove 11
+    cards.append 1
+  return sum(cards)
+    
+
+
 player_card = []
 dealer_card = []
 for _ in range(2):
