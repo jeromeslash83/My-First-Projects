b9d8320963ce3a0b1bb3a713dbbf5ac25b086479
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-04 14:51:34 +0800

    Create AppBreweryVersion.py

---
+++ basic_games/Number_guessing/AppBreweryVersion.py
@@ -0,0 +1,42 @@
+from random import randint
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+turns = 0
+def check_answer(guess, answer, turns):
+  """ Checks answer against guess and checks number of guesses remaining."""
+  if guess > answer:
+    print("Too high.")
+    return turns - 1
+  elif guess < answer:
+    print("Too low.")
+    return turns -1
+  else:
+    print(f"Correct. The answer is {answer}.")
+
+def set_difficulty():
+  level = input("Select difficulty: 'easy' or 'hard'\n")
+  if level == 'easy':
+    return EASY_LEVEL_TURNS
+  else:
+    return HARD_LEVEL_TURNS
+     
+def game():    
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+
+  answer = randint(1, 100)
+  turns = set_difficulty()
+  guess = 0
+  while guess!= answer:
+    guess = int(input("Maka a guess: "))
+    print(f"You have {turns} guesses.")
+    turns = check_answer(guess, answer, turns)
+    print(f"you have {turns} guesses left.")
+    if turns == 0:
+      print("You've run out of guesses. You lose")
+      return
+
+game()
+    
