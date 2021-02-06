d14efa1db068368b98714fa590ecdadf971fcff6
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-06 17:46:33 +0800

    Update Tic_Tac_Toe.py

--- basic_games/Tic_Tac_Toe.py
+++ basic_games/Tic_Tac_Toe.py
@@ -3,7 +3,8 @@
 board = {'1': ' ', '2': ' ', '3': ' ', '4': ' ', '5': ' ',
 '6': ' ', '7': ' ', '8': ' ', '9': ' '
 }
-
+tile_count = 0
+start_game = True
 def tic_board():
     print(f"| {board['1']} | {board['2']} | {board['3']} |")    
     print(f"----+---+----")
@@ -17,36 +18,43 @@ def check_winner():
             print ("Player 1 wins!")
         else:
             print ("Player 2 wins!")
+        return False
     elif board['4'] and board['5'] and board['6'] == 'X' or board['4'] and board['5'] and board['6'] == 'O':
         if board['1'] == 'X':
             print ("Player 1 wins!")
         else:
             print ("Player 2 wins!")
+        return False
     elif board['7'] and board['8'] and board['9'] == 'X' or board['7'] and board['8'] and board['9'] == 'O':
         if board['1'] == 'X':
             print ("Player 1 wins!")
         else:
             print ("Player 2 wins!")
+        return False
     elif board['1'] and board['4'] and board['7'] == 'X' or board['1'] and board['4'] and board['7'] == 'O':
         if board['1'] == 'X':
             print ("Player 1 wins!")
         else:
             print ("Player 2 wins!")
+        return False
     elif board['2'] and board['5'] and board['8'] == 'X' or board['2'] and board['5'] and board['8'] == 'O':
         if board['1'] == 'X':
             print ("Player 1 wins!")
         else:
             print ("Player 2 wins!")
+        return False
     elif board['3'] and board['6'] and board['9'] == 'X' or board['3'] and board['6'] and board['9'] == 'O':
         if board['1'] == 'X':
             print ("Player 1 wins!")
         else:
             print ("Player 2 wins!")
+        return False
     elif board['1'] and board['5'] and board['9'] == 'X' or board['1'] and board['5'] and board['9'] == 'O':
         if board['1'] == 'X':
             print ("Player 1 wins!")
         else:
             print ("Player 2 wins!")
+        return False
     elif board['3'] and board['5'] and board['7'] == 'X' or board['3'] and board['5'] and board['7'] == 'O':
         if board['1'] == 'X':
             print ("Player 1 wins!")
@@ -54,11 +62,11 @@ def check_winner():
             print ("Player 2 wins!")
     elif tile_count == 9:
         print("No more spaces to be filled. It's a tie.")
-
+        return False
+   
 
 print("Welcome to Tic Tac Toe!")
-tile_count = 0
-start_game = True
+
 game = input("Would you like to play a game? 'y' or 'n'\n").lower()
 if game == 'y':
     start_game = True
@@ -70,7 +78,7 @@ while start_game:
     #Puts X and O in the squares in the board
     player_1 = input("Player 1 pick a tile number from 1 to 9: ")
     while board[player_1] != ' ':
-        print('That space is already taker. Try again.')
+        print('That space is already taken. Try again.')
         player_1 = input("Player 1 pick a tile number from 1 to 9: ")
 
     if int(player_1) in range (1,9):
@@ -79,7 +87,9 @@ while start_game:
     else:
         print("Wrong input try again.")
     tic_board()
-    check_winner()
+    if tile_count >= 5:
+        if check_winner() == False:
+            break
 
     player_2 = input("Player 2 pick a tile number from 1 to 9: ")
     while board[player_2] != ' ':
@@ -91,6 +101,12 @@ while start_game:
         tile_count += 1
     else:
         print("Wrong input try again.")
-    check_winner()
+    
+    if tile_count >= 5:
+        if check_winner() == False:
+            start_game = False
+
+        
+
 
 
