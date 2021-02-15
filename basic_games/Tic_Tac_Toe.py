8c18d9349e7733abdc5bf7d0b29fa6ce764ef38d
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-02-15 15:44:53 +0800

    Update Tic_Tac_Toe.py

--- basic_games/Tic_Tac_Toe.py
+++ basic_games/Tic_Tac_Toe.py
@@ -1,112 +1,91 @@
-# TIC TAC TOE GAME
-
-board = {'1': ' ', '2': ' ', '3': ' ', '4': ' ', '5': ' ',
-'6': ' ', '7': ' ', '8': ' ', '9': ' '
-}
-tile_count = 0
 start_game = True
-def tic_board():
+
+
+def tic_board(board):
     print(f"| {board['1']} | {board['2']} | {board['3']} |")    
     print(f"----+---+----")
     print(f"| {board['4']} | {board['5']} | {board['6']} |")
     print(f"----+---+----")
     print(f"| {board['7']} | {board['8']} | {board['9']} |")
 
-def check_winner():
-    if board['1'] and board['2'] and board['3'] == 'X' or board['1'] and board['2'] and board['3'] == 'O':
-        if board['1'] == 'X':
-            print ("Player 1 wins!")
+
+def check_winner(count, board):
+    """Checks the count number of the game and checks if there is a horizontal, vertical, or diagonal line
+    of thee of 'X' or 'O' in the board and returns True if the user has won or is a tie; else it returns False."""
+    patterns = [['1','2','3'], ['4','5','6'], ['7','8','9'], ['1','4','7'], ['2','5','8'], ['3','6','9'],['1','5','9'],['3','5','7']]
+    if count < 5:
+        pass
+    elif count == 9:
+        print("It's a tie!")
+        return True
+    else:
+        for lists in patterns:
+            if set(lists) <= set([k for k,v in board.items() if v == 'X']):
+                print("Player 1 wins!")
+                return True
+            elif set(lists) <= set([k for k,v in board.items() if v == 'O']):
+                print("Player 2 wins!")
+                return True
+            else:
+                pass
+
+
+def new_game():
+    """Restarts or closes the game depending on the user's input"""
+    restart = input("Do you want to play another game? 'y' or 'n'\n").lower()
+    if restart == 'y':
+        tic_tac_toe()
+    else:
+        pass
+
+
+def tic_tac_toe():
+    """Starts the game"""
+    board = {'1': ' ', '2': ' ', '3': ' ', '4': ' ', '5': ' ',
+    '6': ' ', '7': ' ', '8': ' ', '9': ' '
+    }
+    tile_count = 0
+    while start_game:
+
+        tic_board(board)
+        #Puts X and O in the squares in the board
+        player_1 = input("Player 1 pick a tile number from 1 to 9: ")
+        while board[player_1] != ' ':
+            print('That space is already taken. Try again.')
+            player_1 = input("Player 1 pick a tile number from 1 to 9: ")
+
+        if int(player_1) in range (1,9):
+            board[player_1] = 'X'
+            tile_count += 1
         else:
-            print ("Player 2 wins!")
-        return False
-    elif board['4'] and board['5'] and board['6'] == 'X' or board['4'] and board['5'] and board['6'] == 'O':
-        if board['1'] == 'X':
-            print ("Player 1 wins!")
+            print("Wrong input try again.")
+        tic_board(board)
+        if check_winner(tile_count, board):
+            new_game()
+            break
         else:
-            print ("Player 2 wins!")
-        return False
-    elif board['7'] and board['8'] and board['9'] == 'X' or board['7'] and board['8'] and board['9'] == 'O':
-        if board['1'] == 'X':
-            print ("Player 1 wins!")
+            pass
+        
+        player_2 = input("Player 2 pick a tile number from 1 to 9: ")
+        while board[player_2] != ' ':
+            print('That space is already taker. Try again.')
+            player_2 = input("Player 2 pick a tile number from 1 to 9: ")
+            
+        if int(player_2) in range (1,9):
+            board[player_2] = 'O'
+            tile_count += 1
         else:
-            print ("Player 2 wins!")
-        return False
-    elif board['1'] and board['4'] and board['7'] == 'X' or board['1'] and board['4'] and board['7'] == 'O':
-        if board['1'] == 'X':
-            print ("Player 1 wins!")
+            print("Wrong input try again.")
+        if check_winner(tile_count, board):
+            new_game()
+            break
         else:
-            print ("Player 2 wins!")
-        return False
-    elif board['2'] and board['5'] and board['8'] == 'X' or board['2'] and board['5'] and board['8'] == 'O':
-        if board['1'] == 'X':
-            print ("Player 1 wins!")
-        else:
-            print ("Player 2 wins!")
-        return False
-    elif board['3'] and board['6'] and board['9'] == 'X' or board['3'] and board['6'] and board['9'] == 'O':
-        if board['1'] == 'X':
-            print ("Player 1 wins!")
-        else:
-            print ("Player 2 wins!")
-        return False
-    elif board['1'] and board['5'] and board['9'] == 'X' or board['1'] and board['5'] and board['9'] == 'O':
-        if board['1'] == 'X':
-            print ("Player 1 wins!")
-        else:
-            print ("Player 2 wins!")
-        return False
-    elif board['3'] and board['5'] and board['7'] == 'X' or board['3'] and board['5'] and board['7'] == 'O':
-        if board['1'] == 'X':
-            print ("Player 1 wins!")
-        else:
-            print ("Player 2 wins!")
-    elif tile_count == 9:
-        print("No more spaces to be filled. It's a tie.")
-        return False
-   
+            pass
 
 print("Welcome to Tic Tac Toe!")
 
 game = input("Would you like to play a game? 'y' or 'n'\n").lower()
 if game == 'y':
-    start_game = True
+    tic_tac_toe()
 else:
     start_game = False
-
-while start_game:
-    tic_board()
-    #Puts X and O in the squares in the board
-    player_1 = input("Player 1 pick a tile number from 1 to 9: ")
-    while board[player_1] != ' ':
-        print('That space is already taken. Try again.')
-        player_1 = input("Player 1 pick a tile number from 1 to 9: ")
-
-    if int(player_1) in range (1,9):
-        board[player_1] = 'X'
-        tile_count += 1
-    else:
-        print("Wrong input try again.")
-    tic_board()
-    if tile_count >= 5:
-        if check_winner() == False:
-            break
-
-    player_2 = input("Player 2 pick a tile number from 1 to 9: ")
-    while board[player_2] != ' ':
-        print('That space is already taker. Try again.')
-        player_2 = input("Player 2 pick a tile number from 1 to 9: ")
-        
-    if int(player_2) in range (1,9):
-        board[player_2] = 'O'
-        tile_count += 1
-    else:
-        print("Wrong input try again.")
-    
-    if tile_count >= 5:
-        if check_winner() == False:
-            start_game = False
-
-        
-
-
-
